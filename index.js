526f091351952fee93a4a4077eee16b4a3c10754
TwinningGamers <twinninggamersyt@gmail.com>
2025-03-16 13:42:43 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+var password = "Madsthecrusher";
+
+function passcheck() {
+    if(document.getElementById('pass1').value != password) {
+        alert('Wrong Password, Try Again.');
+        return false;
+    }
+    if(document.getElementById('pass1').value == password) {
+        alert('Correct Password. Click OK to enter Webpage');
+    }
+}
